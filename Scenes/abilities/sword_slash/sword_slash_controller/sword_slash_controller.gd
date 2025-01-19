a33052025cb4172556bec52defe90605412c392d
Z-Ahmad <zakiahmad42661@gmail.com>
2025-01-19 18:31:12 -0500

    Enhance enemy grouping and sword slash ability functionality
    - Added a global group for enemies in the project configuration.
    - Updated enemy nodes in the grass scene to belong to the "enemy" group.
    - Modified sword_slash_controller to filter and target enemies within a specified range.

--- Scenes/abilities/sword_slash/sword_slash_controller/sword_slash_controller.gd
+++ Scenes/abilities/sword_slash/sword_slash_controller/sword_slash_controller.gd
@@ -1,5 +1,7 @@
 extends Node2D
 
+
+@export var max_range: float 
 @export var sword_ability: PackedScene
 
 # Called when the node enters the scene tree for the first time.
@@ -12,6 +14,23 @@ func on_timer_timeout():
 	var player = get_tree().get_first_node_in_group("player") as Node2D
 	if player == null:
 		return
-
+		
+	var player_pos = player.global_position
+	
+	var enemies = get_tree().get_nodes_in_group("enemy")
+	enemies = enemies.filter(func(enemy: Node2D): 
+		return enemy.global_position.distance_squared_to(player_pos) < pow(max_range,2)
+	)
+	
+	if enemies.size() == 0:
+		return
+		
+	enemies.sort_custom(func(a: Node2D, b:Node2D):
+		var a_distance = a.global_position.distance_squared_to(player_pos)
+		var b_distance = b.global_position.distance_squared_to(player_pos)
+		return a_distance < b_distance
+	)
+		
 	var sword_instance = sword_ability.instantiate() as Node2D
-	player.add_child(sword_instance)
+	player.get_parent().add_child(sword_instance)
+	sword_instance.global_position = enemies[0].global_position
